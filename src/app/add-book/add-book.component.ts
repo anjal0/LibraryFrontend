594f267a110ff26a7186a6d8919ec43bf4aec6fa
anjal0 <anjalispushparaj@gmail.com>
2021-04-19 16:09:04 +0530

    LibFrontend

---
+++ src/app/add-book/add-book.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookModel } from '../books/book.model';
+import { BooksService } from '../books.service';
+
+@Component({
+  selector: 'app-add-book',
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.css']
+})
+export class AddBookComponent implements OnInit {
+ 
+
+  constructor(private bookService: BooksService, private router: Router) { }
+  bookItem = new BookModel(null,null,null,null);
+  ngOnInit(): void {
+  }
+
+  addBook(){
+    this.bookService.newBook(this.bookItem);
+    this.router.navigate(['/books']);
+  }
+  read(event){
+    var str = (<HTMLInputElement>document.getElementById("input")).value;
+    console.log(str.slice(12));
+    this.bookItem.Cover = str.slice(12);
+  }
+}
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { BookModel } from './book.model';
+import { BooksService } from '../books.service';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  books: BookModel[];
+  
+  
+  constructor(private bookService: BooksService) { }
+
+  ngOnInit(): void {
+    this.bookService.getBooks().subscribe((data)=>{
+      // console.log(data);
+      this.books = JSON.parse(JSON.stringify(data));
+    })
+  }
+
+}
